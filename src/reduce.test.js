dd6412a0ae4156fdc79d97c908589192838a6ed0
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-08 18:05:15 +0300

    res

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,12 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
+  it('Func should return sum of all elements when used with sum callback '
+    + 'and without second parameter', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const callBack = jest.fn((res, el) => res + el);
 
+    expect(arr.reduce(callBack)).toBe(15);
+    expect(callBack).toHaveBeenCalledTimes(arr.length - 1);
   });
-
-  // Add tests here
 });
